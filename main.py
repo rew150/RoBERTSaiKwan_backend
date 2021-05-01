4bb4fba713cca15ed5daa5493faea2f0ecfa35ea
Punyawat Rujipiranunt <rew150@outlook.com>
2021-05-01 13:48:35 +0700

    fix version of fastapi

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/hello")
 async def root():
     return {"message": "Hello, World"}
+
+app.mount("/", StaticFiles(directory='public', html=True), name="static")
