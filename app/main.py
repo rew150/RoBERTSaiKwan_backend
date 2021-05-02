63b359726801e17048243f937d5065450a158a2e
Punyawat Rujipiranunt <rew150@outlook.com>
2021-05-02 17:08:35 +0700

    add model v3

--- app/main.py
+++ app/main.py
@@ -3,8 +3,13 @@ from fastapi import FastAPI, HTTPException
 from fastapi.params import Depends
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm.session import Session
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from .database import SessionLocal, engine
 from . import crud_utils, models, schemas
+from .model.wangchanberta_qa import predict
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -39,5 +44,17 @@ def create_news(news: schemas.News, db: Session = Depends(get_db)):
     if db_news:
         raise HTTPException(status_code=409, detail='Name conflict')
     return crud_utils.create_news(db, news=news)
+
+@app.get('/ex')
+def ex():
+    aaa = '''ราชกิจจานุเบกษา เผยแพร่ข้อกำหนด-มาตรการควบคุมพื้นที่ทั่วประเทศใหม่ พื้นที่ควบคุมสูงสุดและเข้มงวด 6 จังหวัดหนักสุด มีผลบังคับใช้ตั้งแต่ 1 พ.ค. นี้เป็นต้นไป เผยกรณีพบคนไม่สวมใส่หน้ากาก เจ้าหน้าที่สามารถกล่าวตักเตือนได้ ก่อนที่จะใช้กฎหมายตามพระราชบัญญัติโรคติดต่อ พ.ศ.2558
+
+เมื่อวันที่ 29 เมษายน 2564  ผู้สื่อข่าวรายงานว่า เว็บไชต์ราชกิจจานุเษกษาได้ เผยแพร่ ข้อกำหนดออกตาความในมาตรา 9 แห่งพระราชกำหนดการบริหารราชการในสถานการณ์ฉุกเฉิน พ.ศ.2548 (ฉบับที่ 22)
+
+ประกาศฉบับดังกล่าวมีสาระสำคัญตามที่ ศูนย์บริหารสถานการณ์แพร่ระบาดของโรคติดต่อเชื้อไวรัสโคโรนา 2019 (ศบค.) ศบค.แถลงในวันนี้ โดยยกระดับมาตรการการควบคุมการแพร่ระบาดของโรคโควิด-19 ใหม่ หลังสถานการณ์การแพร่ระบาดยังเพิ่มขึ้นอย่างต่อเนื่อง โดยเฉพาะพื้นที่กทม.และปริมณฑล
+
+โดยกำหนดพื้นที่ควบคุมใหม่ เพื่อปรับระดับการกำหนดพื้นที่และ มาตรการการบังคับใช้ คือ 1.พื้นที่ควบคุมสูงสุดและเข้มงวด 6 จังหวัด (สีแดงเข้ม) ได้แก่พื้นที่กรุงเทพมหานคร(กทม.) ชลบุรี นนทบุรี เชียงใหม่ สมุทรปราการ ปทุมธานี 2.พื้นที่ควบคุมสูงสุด (สีแดง) 45 จังหวัด และ 3.พื้นที่ควบคุม (สีส้ม) 26 จังหวัด
+'''
+    return predict(aaa)
 
 app.mount("/", StaticFiles(directory='public', html=True), name="static")
---
+++ app/model/wangchanberta_qa.py
@@ -0,0 +1,61 @@
+from transformers import (
+  default_data_collator,
+  AutoModelForQuestionAnswering,
+  AutoTokenizer,
+  Trainer,
+)
+import numpy as np
+import pandas as pd
+from datasets import load_dataset
+
+model = AutoModelForQuestionAnswering.from_pretrained('./data/v3')
+model_name = "wangchanberta-base-att-spm-uncased" 
+tokenizer = AutoTokenizer.from_pretrained(
+                f'airesearch/{model_name}',
+                revision='main',
+                model_max_length=416,)
+
+data_collator = default_data_collator
+
+trainer = Trainer(
+  model,
+  data_collator=data_collator,
+  tokenizer=tokenizer,
+)
+
+def post_process(data, raw_predictions, tokenizer, n_best_size = 20, max_answer_length=50):
+  all_start_logits, all_end_logits = raw_predictions
+  predictions = []
+  for start_logits, end_logits, example in zip(all_start_logits, all_end_logits, data):
+    start_indexes = np.argsort(start_logits)[-1 : -n_best_size - 1 : -1].tolist()
+    end_indexes = np.argsort(end_logits)[-1 : -n_best_size - 1 : -1].tolist()
+    valid_answers = []
+    for start_index in start_indexes:
+      for end_index in end_indexes:
+          # Don't consider answers with a length that is either < 0 or > max_answer_length.
+          if end_index < start_index or end_index - start_index + 1 > max_answer_length:
+              continue
+          valid_answers.append(
+              {
+                  "score": start_logits[start_index] + end_logits[end_index],
+                  "text": tokenizer.decode(example['input_ids'][start_index:end_index], skip_special_tokens=True)
+              }
+          )
+    if len(valid_answers) > 0:
+        best_answer = sorted(valid_answers, key=lambda x: x["score"], reverse=True)[0]
+    else:
+        # In the very rare edge case we have not a single non-null prediction, we create a fake prediction to avoid failure.
+        best_answer = {"text": "", "score": 0.0} 
+    predictions.append(best_answer["text"])
+  return predictions
+def predict(texts):
+  inp_ids = tokenizer(texts, max_length=416, truncation=True, padding='max_length')['input_ids']
+  att_mask = tokenizer(texts, max_length=416, truncation=True, padding='max_length')['attention_mask']
+  res = pd.DataFrame(columns=['attention_mask', 'input_ids'])
+  res = res.append({'attention_mask': att_mask, 
+                    'input_ids': inp_ids}, ignore_index=True)
+  res.to_json('text.json', orient='records', lines=True)
+  dataset = load_dataset('json', data_files='text.json')
+  raw_predictions = trainer.predict(dataset['train'])
+  predictions = post_process(dataset['train'], raw_predictions[0], tokenizer)
+  return predictions[0]
